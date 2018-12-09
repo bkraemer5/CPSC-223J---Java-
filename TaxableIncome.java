b9a48ddec15bf6dc8d6bd8ac7e4ca8d152fc45c1
Brittany Kraemer <kraemer.brittany707@gmail.com>
2018-12-09 15:46:19 -0800

    Add files via upload

---
+++ TaxableIncome.java
@@ -0,0 +1,59 @@
+/*
+ * Brittany Kraemer		09/04/18
+ * CPSC 223J - Professor Hamidi
+ * Lab 01: Input/Output
+ * This code takes in the users income and calculates the amount of tax they owe
+ */
+
+package damo2;
+
+import javax.swing.JOptionPane;
+/*
+public class TaxableIncome {
+
+	public static void main(String[] args) {
+		
+		String incomeString;
+		int income;
+		double tax = 0;
+		
+		incomeString = JOptionPane.showInputDialog(null, "Enter your Taxable Income:", "Taxable Income", JOptionPane.QUESTION_MESSAGE);
+		
+		income = Integer.parseInt(incomeString);
+		
+		if (income <= 9325) {
+			tax = income*.1;
+			
+		}
+		else if (income <= 37950) {
+			tax = (932.50 + income*.15);
+			
+		}
+		else if (income <= 91900) {
+			tax = (5226.25 + income*.25);
+			
+		}
+		else if (income <= 191650 ) {
+			tax = (18713.75 + income*.28);
+			
+		}
+		else if (income <= 416700) {
+			tax = (46643.75 + income*.33);
+			
+		}
+		else if (income <= 416700) {
+			tax = (120910.25 + income*.35);
+			
+		}
+		else if (income > 418400) {
+			tax = (121505.25 + income*(0.396));
+			
+		}
+		
+		
+		JOptionPane.showMessageDialog(null, "Tax Owed: " + tax);
+		
+	}
+
+}
+*/
